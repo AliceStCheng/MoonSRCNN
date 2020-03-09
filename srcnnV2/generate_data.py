e486e1874650c7a49893851606384cdc517ff02c
Chris Arridge <c.arridge@lancaster.ac.uk>
2020-03-09 10:22:25 +0000

    Updated generate_data.py to mirror the prepare.py script, but also to improve the efficiency of the code.

--- srcnnV2/generate_data.py
+++ srcnnV2/generate_data.py
@@ -1,12 +1,22 @@
-"""Generate training/validation/test data from moon surface image.
+"""Generate training/validation/test data from moon surface image.  This version
+is adapted from prepare.py to put the data sets into an HDF5 file.
 
 Image from https://astrogeology.usgs.gov/search/map/Moon/Clementine/UVVIS/Lunar_Clementine_UVVIS_750nm_Global_Mosaic_118m_v2
 """
 
-import PIL.Image
-import numpy as np
-import argparse
 
+import argparse
+import platform
+import datetime
+import os
+
+import numpy as np
+
+import h5py
+import PIL.Image
+
+# example invocation:
+# python generate_data.py ../Lunar_Clementine_UVVIS_750nm_Global_Mosaic_1.2km_v2.1.png 1000 500 500 -u 4 -ip ../images/ -p ../
 
 if __name__ == '__main__':
 	# Parse arguments.
@@ -20,9 +30,8 @@ if __name__ == '__main__':
 	parser.add_argument('-t', dest='top_cut_fraction', type=float, default=0.2, help='Fraction of the top (NH) of the image to ignore')
 	parser.add_argument('-b', dest='bottom_cut_fraction', type=int, default=0.8, help='Fraction of the bottom (SH) of the image to ignore')
 	parser.add_argument('-r', dest='seed', type=int, default=18732467, help='Random seed')
-	parser.add_argument('-f', dest='output_path', type=str, default='../dataset4/', help='Path to output files.')
-	parser.add_argument('-l', dest='gen_lr', type=bool, default=False, help='Generate low resolution (LR) counterpart images')
-	parser.add_argument('-bic', dest='gen_bic', type=bool, default=False, help='Generate bicubic upsampled images')
+	parser.add_argument('-ip', dest='png_output_path', type=str, default=None, help='Path to output PNG versions of the images.')
+	parser.add_argument('-p', dest='output_path', type=str, default='./', help='Output path')
 	args = parser.parse_args()
 
 	datasets = {'training':args.num_training_samples,
@@ -41,6 +50,44 @@ if __name__ == '__main__':
 
 	# Generate the samples.
 	for key in datasets.keys():
+
+		# Open the HDF file for writing, setup two datasets (one for the LR
+		# images and one for the HR images), and also add some meta data
+		# to the dataset so we can go back at a later date and see what
+		# was used to generate the data.
+		h5_file = h5py.File(os.path.join(args.output_path, key+'.hdf'), 'w')
+		h5_lr_data = h5_file.create_dataset('lr', (datasets[key],args.sample_size,args.sample_size),
+									chunks=(1,args.sample_size,args.sample_size),
+									dtype=np.float32)
+		h5_hr_data = h5_file.create_dataset('hr', (datasets[key],args.sample_size,args.sample_size),
+									chunks=(1,args.sample_size,args.sample_size),
+									dtype=np.float32)
+		h5_file.attrs['map'] = args.file
+		h5_file.attrs['date'] = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+		h5_file.attrs['platform'] = platform.platform()
+		h5_file.attrs['processor'] = platform.processor()
+		h5_file.attrs['python_version'] = platform.python_version()
+		h5_file.attrs['num_samples'] = datasets[key]
+		h5_file.attrs['seed'] = args.seed
+		h5_file.attrs['upscale'] = args.upscale
+		h5_file.attrs['sample_size'] = args.sample_size
+		h5_file.attrs['top_cut_fraction'] = args.top_cut_fraction
+		h5_file.attrs['bottom_cut_fraction'] = args.bottom_cut_fraction
+
+		# If the user asked us to also generate a whole set of PNG files
+		# then make sure all the directories exist, if they don't, then create
+		# them.
+		if args.png_output_path is not None:
+			if not os.path.isdir(args.png_output_path):
+				os.mkdir(args.png_output_path)
+
+			img_path = os.path.join(args.png_output_path,key)
+			if not os.path.isdir(img_path):
+				os.mkdir(img_path)
+		else:
+			img_path = None
+
+		# Here we actually generate all the images.
 		for i in range(datasets[key]):
 			print('Generating {} image {:5d}/{:5d}'.format(key, i+1, datasets[key]))
 
@@ -48,29 +95,27 @@ if __name__ == '__main__':
 			ix = np.random.randint(w - args.sample_size)
 			iy = np.random.randint(hprime - args.sample_size) + h0
 
-			# Cut out the high resolution sample and generate a low resolution
+			# Cut out the high resolution sample (img_hr), generate a low
+			# resolution version that has been bicubic upsampled to the original
+			# sample size.
 			img_hr = img.crop([ix,iy,ix+args.sample_size,iy+args.sample_size])
-			# Generate an upsampled image from bicubic interpolation (4x resolution)
-			if args.gen_bic:
-				img_bic = img_hr.resize((int(args.sample_size*args.upscale),
-										int(args.sample_size*args.upscale)),
-										resample=PIL.Image.BICUBIC)
+			img_lr = img_hr.resize((int(args.sample_size/args.upscale),
+									int(args.sample_size/args.upscale)),
+									resample=PIL.Image.BICUBIC)
+			img_lr = img_lr.resize((args.sample_size,args.sample_size),
+									resample=PIL.Image.BICUBIC)
 
-			# version for further use.
-			if args.gen_lr:
-				img_lr = img_hr.resize((int(args.sample_size/args.upscale),
-										int(args.sample_size/args.upscale)),
-										resample=PIL.Image.BICUBIC)
+			# Write the images to the HDF file.
+			h5_lr_data[i,:,:] = np.array(img_lr)[:,:,0].astype(np.float32)
+			h5_hr_data[i,:,:] = np.array(img_hr)[:,:,0].astype(np.float32)
 
-			# Save.
-			filename = args.output_path+key+'/{:04d}_hr.png'.format(i)
-			with open(filename,'wb') as fh:
-				img_hr.save(fh)
-			if args.gen_lr:
-				filename = args.output_path+key+'/{:04d}_lr.png'.format(i)
-				with open(filename,'wb') as fh:
+			# Save PNGs if we have been asked to do so.
+			if args.png_output_path is not None:
+				with open(os.path.join(img_path,'{:04d}_hr.png'.format(i)),'wb') as fh:
+					img_hr.save(fh)
+
+				with open(os.path.join(img_path,'{:04d}_lr.png'.format(i)),'wb') as fh:
 					img_lr.save(fh)
-			if args.gen_bic:
-				filename = args.output_path+key+'/{:04d}_bic.png'.format(i)
-				with open(filename,'wb') as fh:
-					img_bic.save(fh)
+
+		# Close the HDF file we've just created.
+		h5_file.close()
